5656b67f73fca86ee9528f9a780cee1b853017ad
SJ2008 <SheenJiang@gmail.com>
2024-01-04 16:10:39 -0500

    Update polyline.py

--- lmi_utils/label_utils/polyline.py
+++ lmi_utils/label_utils/polyline.py
@@ -1,4 +1,3 @@
-#SJ: added 1/4/2024
 from label_utils.shape import Shape
 
 class Polyline(Shape):
@@ -19,4 +18,3 @@ class Polyline(Shape):
         self.X = x_vals
         self.Y = y_vals
         self.confidence = confidence
-#SJ: END
